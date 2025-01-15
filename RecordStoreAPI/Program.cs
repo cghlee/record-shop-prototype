0d831f2a3a5f47fde9ea746ce216cb6dd845307b
Christopher Lee <leecgh@gmail.com>
2025-01-15 10:16:53 +0000

    Add production appsettings JSON file with user-defined connection string

--- RecordStoreAPI/Program.cs
+++ RecordStoreAPI/Program.cs
@@ -21,7 +21,7 @@ public class Program
         if (builder.Environment.IsDevelopment())
         {
             builder.Services.AddDbContext<AlbumsDbContext>(options =>
-                options.UseInMemoryDatabase("TestDb"));
+                options.UseInMemoryDatabase("TestInMemoryDb"));
         }
         else
         {
